6ae14a81dc55d186ba1a7c3cfbc11e984ce63ed2
Soundless <mohammad0sharifi0sadeghi@gmail.com>
2021-04-29 14:49:22 +0430

    RemoveTime Added

---
+++ Application/Features/Time/Command/RemoveTime/RemoveTimeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Features.Time.Command.RemoveTime
+{
+    public class RemoveTimeCommand : IRequest<Unit>
+    {
+        public int CourseId { get; set; }
+        public int TimeId { get; set; }
+    }
+}
---
+++ Application/Features/Time/Command/RemoveTime/RemoveTimeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Resources;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Features.Time.Command.RemoveTime
+{
+    public class RemoveTimeCommandValidator : AbstractValidator<RemoveTimeCommand>
+    {
+        public RemoveTimeCommandValidator(IStringLocalizer<SharedResource> localizer)
+        {
+            RuleFor(r => r.CourseId)
+                .NotEmpty();
+            RuleFor(r => r.TimeId)
+                .NotEmpty();
+        }
+    }
+}
