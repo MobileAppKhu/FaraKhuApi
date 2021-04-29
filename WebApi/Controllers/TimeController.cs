8823d424a8729213f1f68394d38f2f381273deea
Mehrad Moshiri <mehradmoshiri09@gmail.com>
2021-04-29 18:56:02 +0430

    'hassan'

---
+++ WebApi/Controllers/TimeController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Time.Command.AddTime;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private IMediator _mediator;
+        TimeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        [Authorize(Policy = "InstructorPolicy")]
+        [ProducesResponseType(typeof(AddTimeCommand), 200)]
+        public async Task<IActionResult> AddTime(AddTimeCommand request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        
+    }
+}
