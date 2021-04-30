49414e951809333756ead1152480dee2defb1010
Mehrad Moshiri <mehradmoshiri09@gmail.com>
2021-04-30 17:41:17 +0430

    Add Remove Announcement

---
+++ Application/Features/Announcement/Commands/RemoveAnnouncement/RemoveAnnouncementCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+
+namespace Application.Features.Announcement.Commands.RemoveAnnouncement
+{
+    public class RemoveAnnouncementCommand : IRequest<RemoveAnnouncementViewModel>
+    {
+        public int AnnouncementId { get; set; }
+    }
+}
