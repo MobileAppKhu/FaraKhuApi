4a507cc15639b3184d65ebc6a882c05d23468cf0
Mehrad Moshiri <mehradmoshiri@khu.ac.ir>
2021-12-17 22:54:50 +0330

    Add Utilities and Extensions

---
+++ Utilities/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> GetPage<T>(this IEnumerable<T> list, int pageNumber, int itemsPerPage = 5)
+        {
+            return list.Skip((pageNumber - 1) * itemsPerPage).Take(itemsPerPage);
+        }
+    }
+}
