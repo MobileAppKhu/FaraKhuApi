d5ff47f40370738ac0364ef0893d32a36e9e388a
Mehrad Moshiri <mehradmoshiri@khu.ac.ir>
2021-12-17 22:52:43 +0330

    add AddCommentCommand

---
+++ Application/Features/News/Commands/AddComment/AddCommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.News;
+
+namespace Application.Features.News.Commands.AddComment
+{
+    public class AddCommentViewModel
+    {
+        public bool Status { get; set; } 
+    }
+}
