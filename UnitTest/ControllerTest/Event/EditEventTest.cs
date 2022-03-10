0f73159abc2d050e2b482c97a43bb0c71c581026
AlirezaDeghy <AlirezaDehghan0098@gmail.com>
2022-03-10 11:08:50 +0330

    Edit Event Test Compelete

---
+++ UnitTest/ControllerTest/Event/EditEventTest.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Application.Features.Event.Commands.EditEvent;
+using Microsoft.AspNetCore.TestHost;
+using UnitTest.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTest.ControllerTest.Event
+{
+    public class EditEventTest : AppFactory
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        private readonly string _path = "api/Event/EditEvent";
+        
+        public EditEventTest(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        
+        [Fact]
+        public async Task EditEventName_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                EventName = "EditedEventName"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEventDescription_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                EventDescription = "EditedEventDescription"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEventTime_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                EventTime = DateTime.Today.ToString()
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEventIsDone_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                changingIsDone = true
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEventCourseId_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                CourseId = "EditedCourseId"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEvent_AnotherUserShouldNotEdit()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToSecondInstructor();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                CourseId = "EditedCourseId"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEvent_AnotherUserTypeShouldNotEdit()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToStudent();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "1",
+                CourseId = "EditedCourseId"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task EditEvent_EventShouldNotBeFound()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToStudent();
+
+            var data = new EditEventCommand()
+            {
+                EventId = "2",
+                CourseId = "EditedCourseId"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+    }
+}
