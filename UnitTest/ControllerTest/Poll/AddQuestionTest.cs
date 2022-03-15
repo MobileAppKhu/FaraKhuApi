5ea415d0fd701e52e2beb2ce74e7fca4f5a2f25b
AlirezaDeghy <AlirezaDehghan0098@gmail.com>
2022-03-15 11:28:08 +0330

    Add Poll Question Test Compelete Name Edited

---
+++ UnitTest/ControllerTest/Poll/AddQuestionTest.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Application.Features.Poll.Commands.AddQuestion;
+using Microsoft.AspNetCore.TestHost;
+using UnitTest.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTest.ControllerTest.Poll
+{
+    public class AddQuestionTest : AppFactory
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        private readonly string _path = "api/Poll/AddPollQuestion";
+        
+        public AddQuestionTest(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        
+        [Fact]
+        public async Task AddQuestion_ShouldWorkCorrectly()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new AddQuestionCommand()
+            {
+                CourseId = "CourseId",
+                MultiVote = "true",
+                QuestionDescription = "Description",
+                Answers = new List<string>()
+                {
+                   "Answer1",
+                   "Answer2"
+                }
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task AddQuestion_ShouldNotCreatOfferWithOutCourseId()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new AddQuestionCommand()
+            {
+                MultiVote = "true",
+                QuestionDescription = "Description",
+                Answers = new List<string>()
+                {
+                   "Answer1",
+                   "Answer2"
+                }
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task AddQuestion_ShouldNotCreatOfferWithOutAnswer()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new AddQuestionCommand()
+            {
+                MultiVote = "true",
+                QuestionDescription = "Description",
+                CourseId = "CourseId"
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task AddQuestion_ShouldNotCreatOfferWithOutDescription()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToInstructor();
+
+            var data = new AddQuestionCommand()
+            {
+                MultiVote = "true",
+                CourseId = "CourseId",
+                Answers = new List<string>()
+                {
+                   "Answer1",
+                   "Answer2"
+                }
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task AddQuestion_ShouldBeUnauthorized()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToStudent();
+
+            var data = new AddQuestionCommand()
+            {
+                MultiVote = "true",
+                CourseId = "CourseId",
+                QuestionDescription = "description",
+                Answers = new List<string>()
+                {
+                   "Answer1",
+                   "Answer2"
+                }
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.True(!await response.HasErrorCode());
+        }
+        
+        [Fact]
+        public async Task AddQuestion_AntoherUserShouldNotAdd()
+        {
+            // Arrange
+            var client = Host.GetTestClient();
+            await client.AuthToSecondInstructor();
+
+            var data = new AddQuestionCommand()
+            {
+                MultiVote = "true",
+                CourseId = "CourseId",
+                QuestionDescription = "description",
+                Answers = new List<string>()
+                {
+                   "Answer1",
+                   "Answer2"
+                }
+            };
+            //Act
+            var response = await client.PostAsync(_path, data);
+            
+            //Output
+            _outputHelper.WriteLine(await response.GetContent());
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.NotAcceptable, response.StatusCode);
+            Assert.True(await response.HasErrorCode());
+        }
+        
+    }
+}
